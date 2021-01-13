bc7b7c28350fade48b4c1156b14e72cd95d643a7
Bulusu Srinivasa Vinay <vbulusu@athenahealth.com>
2021-01-13 16:49:02 +0530

    testing

--- application.py
+++ application.py
@@ -42,19 +42,19 @@ application.config["SQLALCHEMY_DATABASE_URI"] = DB_STRING
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(application)
 
-
+#DB classes
 #user info table for the db
 class Users(db.Model):
-	__tablename__="users_info"
-	id=db.Column('id', db.Integer, primary_key=True)
-	username=db.Column('username', db.String(20), unique=True)
-	email = db.Column('email', db.String(30))
-	password = db.Column('password', db.String(100))
+    __tablename__="users_info"
+    id=db.Column('id', db.Integer, primary_key=True)
+    username=db.Column('username', db.String(20), unique=True)
+    email = db.Column('email', db.String(30))
+    password = db.Column('password', db.String(100))
 
-	def __init__(self, username, email, password):
-		self.username=username
-		self.email=email
-		self.password=password
+    def __init__(self, username, email, password):
+        self.username=username
+        self.email=email
+        self.password=password
 
     def get_reset_token(self, expires_sec=600):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
